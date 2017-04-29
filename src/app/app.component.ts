e5c0322bffe2ee654d4924afbc8aebb12379c893
Peter Oliha <poliha2002@yahoo.com>
2017-04-29 21:38:36 +0100

    added translation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,21 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { TabsPage } from '../pages/tabs/tabs';
+import { Welcome } from '../pages/welcome/welcome';
+import { TranslateService } from 'ng2-translate/ng2-translate';
+
 
 @Component({
   templateUrl: 'app.html'
 })
-export class MyApp {
-  rootPage:any = TabsPage;
+export class Lumania {
+  rootPage:any = Welcome;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(translate: TranslateService, platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    // Set the default language for translation strings, and the current language.
+    translate.setDefaultLang('en');
+    translate.use('en')
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,29 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
+import { Http } from '@angular/http';
+import { Lumania } from './app.component';
 
+import { Welcome } from '../pages/welcome/welcome';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
+
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { TranslateModule, TranslateLoader, TranslateStaticLoader } from 'ng2-translate/ng2-translate';
+
+export function createTranslateLoader(http: Http) {
+  return new TranslateStaticLoader(http, './assets/i18n', '.json');
+}
+
 
 @NgModule({
   declarations: [
-    MyApp,
+    Lumania,
+    Welcome,
     AboutPage,
     ContactPage,
     HomePage,
@@ -21,11 +31,17 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(Lumania),
+    TranslateModule.forRoot({
+      provide: TranslateLoader,
+      useFactory: (createTranslateLoader),
+      deps: [Http]
+    })
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
+    Lumania,
+    Welcome,
     AboutPage,
     ContactPage,
     HomePage,
