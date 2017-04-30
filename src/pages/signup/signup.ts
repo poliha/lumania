b6bfa5b57cdfebdc6a8ee960a6b9e8e0782d943c
Peter Oliha <poliha2002@yahoo.com>
2017-04-30 22:02:29 +0100

    implemented basic sign up logic

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Auth, User, UserDetails, IDetailedError } from '@ionic/cloud-angular';
 
 /**
  * Generated class for the Signup page.
@@ -13,13 +14,57 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'signup.html',
 })
 export class Signup {
-	// account: {name: string, email: string, password: string};
-	account = {};
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+	account: {name: string, email: string, password: string} = {
+		'email': "this.account.email",
+  	'password': "",
+  	'name': ""
+  };
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public auth: Auth, public user: User) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad Signup');
   }
 
+  doSignUp(){
+  	let details: UserDetails = {
+  		'email': this.account.email,
+  		'password': this.account.password,
+  		'name': this.account.name
+  	};
+  	console.log('details: ', details);
+
+  	this.auth.signup(details).then(() => {
+		  alert("user sign up successful");
+		  console.log("user: ",this.user);
+		}, (err: IDetailedError<string[]>) => {
+		  for (let e of err.details) {
+		  	console.log(e);
+		  	// TO DO handle errors
+		    // if (e === 'conflict_email') {
+		    //   // alert('Email already exists.');
+		    // }
+		    // else if(error === 'required_email'){
+      //           // email missing
+      //   }
+      //   else if(error === 'required_password'){
+      //           // password missing
+      //   }
+      //   else if(error === 'conflict_email'){
+      //           // email already in use
+      //   }
+      //   else if (error === 'conflict_username'){
+      //           // username alerady in use
+      //   }
+      //   else if (error === ' invalid_email'){
+      //           // email not valid
+      //   }
+      //   else{
+
+      //   }
+		  }
+		});
+
+  }
 }
