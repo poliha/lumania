1b3000cb657ba21ad982feff972a8ac5043ab420
Peter Oliha <poliha2002@yahoo.com>
2017-05-02 08:55:11 +0100

    added password reset logic

--- src/pages/password-reset/password-reset.ts
+++ src/pages/password-reset/password-reset.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Auth, User} from '@ionic/cloud-angular';
 
 /**
  * Generated class for the PasswordReset page.
@@ -16,13 +17,14 @@ export class PasswordReset {
 
 	email: string;
 	part: string;
-	account: {auth_code: string, password: string, confirm_password: string} = {
-		'auth_code': "",
+	account: {auth_code: number, password: string, confirm_password: string} = {
+		'auth_code': null,
   	'password': "",
   	'confirm_password': ""
   };
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+  		public auth: Auth, public user: User) {
   	this.part = "request";
   }
 
@@ -31,7 +33,21 @@ export class PasswordReset {
   }
 
   requestReset(){
+  	this.auth.requestPasswordReset(this.email).then((d) => {
 
+        // success
+        console.log(d);
+
+    }, (err) => {
+
+   	    // problem logging in
+        console.log(err);
+
+    });;
+  }
+
+  confirmReset(){
+  	this.auth.confirmPasswordReset(this.account.auth_code, this.account.password);
   }
 
 }
