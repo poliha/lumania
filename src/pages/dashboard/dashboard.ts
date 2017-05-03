04f77f32ce05044861be5e6aa99a0bf25846bcd7
Peter Oliha <poliha2002@yahoo.com>
2017-05-03 23:13:36 +0100

    generated dashboard page

---
+++ src/pages/dashboard/dashboard.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AuthService } from '../../providers/auth-service';
+import { Welcome } from '../welcome/welcome';
+import { Rates } from '../rates/rates';
+import { Contacts } from '../contacts/contacts';
+import { Wallet } from '../wallet/wallet';
+import { News } from '../news/news';
+import { Profile } from '../profile/profile';
+
+
+/**
+ * Generated class for the Dashboard page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+@IonicPage()
+@Component({
+  selector: 'page-dashboard',
+  templateUrl: 'dashboard.html',
+})
+export class Dashboard {
+
+  tab1Root = Rates;
+  tab2Root = Contacts;
+  tab3Root = Wallet;
+  tab4Root = News;
+  tab5Root = Profile;
+  
+  constructor(public navCtrl: NavController, public navParams: NavParams, public authService: AuthService) {
+
+    if (this.authService.isLoggedIn()) { 
+      console.log("user: ",this.authService.user);
+    } else {
+      console.log("nt auth");
+      this.navCtrl.push(Welcome);
+    }
+  }
+
+  ionViewCanEnter(){
+    console.log("can enter");
+    return this.authService.isLoggedIn();
+  }
+
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad Dashboard');
+    if (this.authService.isLoggedIn()) { 
+    	console.log("user: ",this.authService.user);
+    } else {
+    	console.log("not logged in");
+    }
+  }
+
+}
